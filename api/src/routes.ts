ee7d762afc0e689dc4450699199d4e7916bc66a8
Paola Monteiro <paoolamonteiro98@gmail.com>
2021-02-28 20:43:33 -0300

    Create necessary routes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
+
 router.post('/users', userController.create);
+router.get('/users', userController.getAll);
+router.delete('/users', userController.deleteAllUsers);
+
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.getAll);
+router.delete('/surveys', surveyController.deleteAllSurveys);
 
 export { router };
